17955e7c96fb8786d256bf0744f9cf2816390e93
sou10202 <souichi.12rou14@gmail.com>
2024-07-23 00:37:00 +0900

    bugFix

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 # include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    for(int i = 0; i < 10; i++) {
+        printf("Hello, World!\n");
+    }
+    // printf("Hello, World!\n");
     return 0;
 }
