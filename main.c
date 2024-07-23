229ed6e68cee14da26e2e39d3c3e678353dff439
sou10202 <souichi.12rou14@gmail.com>
2024-07-23 10:07:49 +0900

    reduce loop counts

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 # include <stdio.h>
 
 int main() {
-    for(int i = 0; i < 10; i++) {
+    for(int i = 0; i < 5; i++) {
         printf("Hello, World!\n");
     }
     // printf("Hello, World!\n");
